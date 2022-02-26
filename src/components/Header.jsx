b86a27b7239852a6b656ca223941e909f8e23f44
nasrah01 <nasrah1@hotmail.co.uk>
2022-02-26 21:50:17 +0000

    filter search input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Search from './SearchBar'
+import { RiShoppingBagLine } from 'react-icons/ri'
 
 const Header = () => {
   return (
     <header>
-      <div>
-        Logo
-      </div>
+      <div>Logo</div>
       <div>
         <Search />
       </div>
+      <div>
+        <div>Sign in</div>
+        <RiShoppingBagLine />
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
-import { IoSearchOutline } from 'react-icons/io'
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
 
-  const onSearchSubmit = (e) => {
-    
-    const searchInput = e.target.value();
-    console.log(searchInput);
-  }
   return (
     <div>
-      <input
-        type="text"
-        id="search"
-        name="search"
-        placeholder="Search product"
-        onChange={onSearchSubmit}
-      />
-      <div>
-        <div>
-          <IoSearchOutline />
-        </div>
-      </div>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search"
+          inputProps={{ "aria-label": "search" }}
+        />
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </div>
   );
 }
