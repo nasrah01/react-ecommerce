f25388927a032257c7c5a0cc8c1ca90d853f637c
nasrah01 <nasrah1@hotmail.co.uk>
2022-03-09 19:52:58 +0000

    homepage grid

--- src/components/HomePicks.jsx
+++ src/components/HomePicks.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import './homePicks.css';
 
 export default function Products() {
   const [products, setProducts] = useState([]);
@@ -23,9 +24,17 @@ export default function Products() {
           <img src={products[0].image} alt={products[0].catagory} />
         </div>
         <div className="mosaic__right">
-          <img src={products[1].image} alt={products[1].catagory} />
-          <img src={products[2].image} alt={products[2].catagory} />
-          <img src={products[3].image} alt={products[3].catagory} />
+          <div>
+            <img src={products[1].image} alt={products[1].catagory} />
+            <img src={products[2].image} alt={products[2].catagory} />
+            <img src={products[3].image} alt={products[3].catagory} />
+          </div>
+          <div>
+            <img src={products[4].image} alt={products[4].catagory} />
+            <img src={products[5].image} alt={products[5].catagory} />
+            <img src={products[6].image} alt={products[6].catagory} />
+            <img src={products[7].image} alt={products[7].catagory} />
+          </div>
         </div>
       </div>
     </div>
