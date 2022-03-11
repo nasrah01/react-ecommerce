426742887cab55c81335b79389b82bd5d781ea1b
nasrah01 <nasrah1@hotmail.co.uk>
2022-03-11 23:47:44 +0000

    homepage grid

--- src/components/HomePicks.jsx
+++ src/components/HomePicks.jsx
@@ -20,21 +20,10 @@ export default function Products() {
     <div>
       <h2>Top Picks</h2>
       <div className="homepage__mosaic">
-        <div className="mosaic__left">
-          <img src={products[0].image} alt={products[0].catagory} />
-        </div>
         <div className="mosaic__right">
-          <div>
-            <img src={products[1].image} alt={products[1].catagory} />
-            <img src={products[2].image} alt={products[2].catagory} />
-            <img src={products[3].image} alt={products[3].catagory} />
-          </div>
-          <div>
-            <img src={products[4].image} alt={products[4].catagory} />
-            <img src={products[5].image} alt={products[5].catagory} />
-            <img src={products[6].image} alt={products[6].catagory} />
-            <img src={products[7].image} alt={products[7].catagory} />
-          </div>
+          {products.map((product) => (
+            <img src={product.image} alt={products.catagory} />
+          ))}
         </div>
       </div>
     </div>
