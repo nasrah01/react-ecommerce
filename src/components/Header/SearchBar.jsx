e31e8a0f6a686934600a5e5a29025158b9ba7848
nasrah01 <nasrah1@hotmail.co.uk>
2022-05-12 20:24:23 +0100

    update search

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from 'react-redux';
 import { filterItems } from "../../redux/reducers/items";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from 'react-router-dom';
@@ -22,23 +20,17 @@ const SearchBar = () => {
 
   return (
     <>
-      <Paper
-        component="form"
-        sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}
-        onSubmit={handleSearchFilter}
-        square
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1, width: 400 }}
+      <form onSubmit={handleSearchFilter} className="search">
+        <input
+          className="search__input"
           placeholder="Search products"
-          inputProps={{ "aria-label": "search products" }}
           value={filterSearch}
           onChange={(e) => setFilterSearch(e.target.value.toLowerCase())}
         />
         <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
           <SearchIcon />
         </IconButton>
-      </Paper>
+      </form>
     </>
   );
 };
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,16 +1,20 @@
 import Products from "../components/Products/Products";
 import { useSelector } from "react-redux";
+import { FcSearch } from 'react-icons/fc';
 
 
 const SearchResult = () => {
 
   const items = useSelector((state) => state.products.filter);
 
-  console.log(items);
   return (
-    <div>
+    <div className="search__results">
       <h2>Search results</h2>
-      {items.length > 0 ? <Products items={items} /> :  <div>Search not found</div>}
+      {items.length > 0 ? <Products items={items} /> :  
+      <div className="search__error">
+        <h3>Oops nothing found! <br /> try again</h3>
+        <FcSearch size={64}/>
+      </div>}
     </div>
   );
 };
